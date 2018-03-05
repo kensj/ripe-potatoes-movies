5b25a4f1392f3e270c857b717283b155a7785109
rlamchops <lamraymond1996@gmail.com>
2018-03-05 16:56:43 -0500

    gui files

---
+++ html/js/profile.js
@@ -0,0 +1,19 @@
+$('[rel="tooltip"]').tooltip({
+	animated: 'fade',
+	placement: 'top',
+	html:true
+});
+$(function() {
+	$('[rel="popover"]').popover({
+		animated:'fade',
+		placement: 'top',
+		html:true
+	});
+})
+function openM() {
+	$("#forgotFrame").hide();
+  $("#registerFrame").hide();
+  $("#loginFrame").hide();
+	$("#manageAccount").show();
+	document.getElementById('id01').style.display='block';
+}
