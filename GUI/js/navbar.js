117e8955bd44f7ae82558effd9abdc4d6c17a632
rlamchops <lamraymond1996@gmail.com>
2018-03-29 22:56:07 -0400

    pseudo search bar autocomplete functionality

--- GUI/js/navbar.js
+++ GUI/js/navbar.js
@@ -39,3 +39,22 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var countries = [
+    { value: 'Moana', data: 'Moana' },
+    { value: 'Big Hero 6', data: 'Big Hero 6' },
+    { value: 'Logan', data: 'Logan' },
+    { value: 'Fantastic Beasts', data: 'Fantastic Beasts' },
+    { value: 'Zootopia', data: 'Zootopia' },
+    { value: 'Robocop', data: 'Robocop' },
+    { value: 'Early Man', data: 'Early Man' },
+    { value: 'Baywatch', data: 'Baywatch' },
+    { value: 'Suicide Squad', data: 'Suicide Squad' }
+];
+
+$('#autocomplete').autocomplete({
+    lookup: countries,
+    onSelect: function (suggestion) {
+        console.log(suggestion.data);
+    }
+});
