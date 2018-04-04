941805651d26a2cc69ff462c3396bed4462f1062
rlamchops <lamraymond1996@gmail.com>
2018-04-04 18:23:18 -0400

    modified inheritance type so as to generate separate tables for all types of content, also removed auto gen attribute for content id

--- Domain_Objects/src/main/java/potatoes/project/domain_objects/Content.java
+++ Domain_Objects/src/main/java/potatoes/project/domain_objects/Content.java
@@ -9,11 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 //import javafx.scene.image.Image;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
@@ -23,6 +26,7 @@ import javax.validation.constraints.NotNull;
  */
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Content {
     
     private boolean isFeatured;
