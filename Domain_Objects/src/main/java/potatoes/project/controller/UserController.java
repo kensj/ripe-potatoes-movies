a7958ca1cef9cd19eb674106086b48f96978fa3e
rlamchops <lamraymond1996@gmail.com>
2018-04-05 22:05:59 -0400

    login seems to work, but need to test session still

--- Domain_Objects/src/main/java/potatoes/project/controller/UserController.java
+++ Domain_Objects/src/main/java/potatoes/project/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import potatoes.project.domain_objects.PasswordAuthentication;
 import potatoes.project.domain_objects.User;
@@ -24,31 +25,55 @@ import potatoes.project.service.UserService;
 import potatoes.project.validator.UserValidator;
 
 @RestController
+@SessionAttributes("user")
 public class UserController {
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private UserValidator userValidator;
-	
-	@Autowired
-	private PasswordAuthentication passwordAuthentication;
 	
 	@PostMapping("/register")
 	public ResponseEntity<?> registerRequest(@Valid @RequestBody User user, Errors errors) {
 		Map<String, String> response = new HashMap<String, String>();
 		
 		if (errors.hasErrors()) {
-			response.put("error", "ajax request failed");
+			response.put("error", "ajax register request failed");
 			return ResponseEntity.badRequest().body(response);
         }
 		
 		
 		if (userService.findByUsername(user.getName()) != null) {
 			response.put("success", "false");
+			response.put("reason", "username");
 		}
 		else {
 			response.put("success", "true");
 			userService.save(user);
+		}
+		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> loginRequest(@Valid @RequestBody User user, Errors errors, Model model) {
+		Map<String,String> response = new HashMap<String, String>();
+		
+		if (errors.hasErrors()) {
+			response.put("error", "ajax login request failed");
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		User targetUser = userService.findByUsername(user.getName());
+		if (targetUser != null) {
+			if (userService.authenticate(user.getName(), user.getPassword())) {
+				response.put("success", "true");
+				model.addAttribute("user", targetUser);
+			}
+			else {
+				response.put("success", "false");
+				response.put("reason", "password");
+			}
+		}
+		else {
+			response.put("success", "false");
+			response.put("reason", "username");
 		}
 		return ResponseEntity.ok(response);
 	}
