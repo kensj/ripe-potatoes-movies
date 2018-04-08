3d742302482a0458efee2d503d342e2443d694d8
rlamchops <lamraymond1996@gmail.com>
2018-04-08 12:19:17 -0400

    updated report to have reportdate and resolved boolean

--- Domain_Objects/src/main/java/potatoes/project/domain_objects/ReportQueue.java
+++ Domain_Objects/src/main/java/potatoes/project/domain_objects/ReportQueue.java
@@ -6,16 +6,30 @@
 package potatoes.project.domain_objects;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Queue;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author kdill
  */
 
+@Entity
 public class ReportQueue {
-    private static LinkedBlockingQueue<Report> reportQueue;
+	@OneToMany
+    private static Queue<Report> reportQueue;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int rqID;
+	
+	public ReportQueue() {
+		reportQueue = new LinkedBlockingQueue<Report>();
+	}
     
     public static int queueReport(Report r){
         reportQueue.add(r);
