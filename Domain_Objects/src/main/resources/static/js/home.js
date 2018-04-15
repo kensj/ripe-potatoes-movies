5f334bcbada9de2a874fff55c792761153ab308f
rlamchops <lamraymond1996@gmail.com>
2018-04-15 18:06:15 -0400

    calendar is now correctly auto filled with the current month and it's days
    Former-commit-id: 6ca5acf3c0215b6df4ee019793799c3bb94f3bed

--- Domain_Objects/src/main/resources/static/js/home.js
+++ Domain_Objects/src/main/resources/static/js/home.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+	$(".recommendedContainer").hide();
+	$(".calendarContainer").hide();
+	populateCalendar();
+});
+
 $('.carousel[data-type="multi"] .item').each(function() {
 	var next = $(this).next();
 	if (!next.length) {
@@ -14,3 +20,45 @@ $('.carousel[data-type="multi"] .item').each(function() {
 		next.children(':first-child').clone().appendTo($(this));
 	}
 });
+
+function showCalendar() {
+	$(".statsContainer").hide();
+	$(".recommendedContainer").hide();
+	$(".calendarContainer").show();
+}
+
+function showStats() {
+	$(".recommendedContainer").hide();
+	$(".calendarContainer").hide();
+	$(".statsContainer").show();
+}
+
+function showRecommended() {
+	$(".calendarContainer").hide();
+	$(".statsContainer").hide();
+	$(".recommendedContainer").show();
+}
+
+//generates the current month
+function populateCalendar() {
+	var date = new Date(),
+	locale = "en-us",
+	month = date.toLocaleString(locale, {month: "long"});
+	//fill current month and year
+	$(".calendarContainer > div > div > ul > li").html(month + "<br><span>" + date.getFullYear() + "</span>");
+	
+	var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+	var lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0);
+	var numDays = lastDay.getDate();
+	var startDay = firstDay.getDay();
+	
+	for (var i = 0; i < startDay; i++) {
+		$(".days").append("<li></li>");
+	}
+	
+	for (var i = 0; i < numDays; i++) {
+		$(".days").append("<li>" + (i + 1) + "</li>");
+	}
+	
+}
+
