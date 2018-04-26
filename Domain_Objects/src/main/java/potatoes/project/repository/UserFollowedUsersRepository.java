58d79f22aadc8840d572fea3c0df564bab2e1399
kensj <kenji.japra@gmail.com>
2018-04-26 13:37:34 -0400

    Spooky
    Former-commit-id: 4034d4a7c18104353421d81119d32c02c98dc920 [formerly aeafe5ea4d579240d931f44993717b9f94f5c84a]
    Former-commit-id: 5aae3fb4d3f0ce49876b99d0e1d324c7a3aa1eaf

--- Domain_Objects/src/main/java/potatoes/project/repository/UserFollowedUsersRepository.java
+++
@@ -1,9 +0,0 @@
-package potatoes.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import potatoes.project.domain_objects.User;
-
-public interface UserFollowedUsersRepository extends JpaRepository<User, Integer> {
-	//List<Integer> findbyUser__UserID(int id);
-}
