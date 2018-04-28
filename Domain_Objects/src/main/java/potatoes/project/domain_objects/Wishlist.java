2e96c01bc3f77adf05e0a7588873fe09e6264e60
kensj <kenji.japra@gmail.com>
2018-04-28 14:09:34 -0400

    Entity Annotations
    Former-commit-id: fb795601875b8edc551981217cc49e3734c9f7a8 [formerly ab686371d90af9432db2e2c089364b4d041dbbf5]
    Former-commit-id: 698b23893b6f9d09973f30d3ef4c1fb308622678

--- Domain_Objects/src/main/java/potatoes/project/domain_objects/Wishlist.java
+++ Domain_Objects/src/main/java/potatoes/project/domain_objects/Wishlist.java
@@ -1,10 +1,12 @@
 package potatoes.project.domain_objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Wishlist {
 	
 	@OneToOne
