a73d78639b81b50b5bc82334ea8dc8a4c794d85e
kensj <kenji.japra@gmail.com>
2018-05-04 11:19:54 -0400

    rating graph
    Former-commit-id: 891ce97fcdb51f99a76ffdb1273f6ddd242f7dc7 [formerly 0e930d178ee53f2545c35148cd1b8ac8c2227001]
    Former-commit-id: 76cb7fc8486d7d41a191cf22e366e63960a43097

--- Domain_Objects/src/main/java/potatoes/project/repository/RatingRepository.java
+++ Domain_Objects/src/main/java/potatoes/project/repository/RatingRepository.java
@@ -20,14 +20,14 @@ public interface RatingRepository extends JpaRepository<Rating, Integer>{
 	@Query("select sum(r.score) from Rating r where r.content=?1")
 	Double getSumScore(Content c);
 	
-	@Query("select r.score from Rating r where r.score>=1 and r.score<1.5 and r.content=?1")
+	@Query("select r.score from Rating r where r.score>=1 and r.score<2 and r.content=?1")
 	List<Rating> findOneStar(Content c);
-	@Query("select r.score from Rating r where r.score>=1.5 and r.score<2.5 and r.content=?1")
+	@Query("select r.score from Rating r where r.score>=2 and r.score<3 and r.content=?1")
 	List<Rating> findTwoStar(Content c);
-	@Query("select r.score from Rating r where r.score>=2.5 and r.score<3.5 and r.content=?1")
+	@Query("select r.score from Rating r where r.score>=3 and r.score<4 and r.content=?1")
 	List<Rating> findThreeStar(Content c);
-	@Query("select r.score from Rating r where r.score>=3.5 and r.score<4.5 and r.content=?1")
+	@Query("select r.score from Rating r where r.score>=4 and r.score<5 and r.content=?1")
 	List<Rating> findFourStar(Content c);
-	@Query("select r.score from Rating r where r.score>=4.5 and r.score<=5 and r.content=?1")
+	@Query("select r.score from Rating r where r.score=5 and r.content=?1")
 	List<Rating> findFiveStar(Content c);
 }
