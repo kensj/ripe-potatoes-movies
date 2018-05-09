5f593a0dd635cc3db68135579f780ed385339eee
BrianGeneWong <brian.gene.wong@gmail.com>
2018-05-09 03:33:57 -0400

    adding filmography class to insert into filmography
    Former-commit-id: 353f3ae39565ebdc3384ab30837cc926b83a5d96 [formerly b37beb3d826c014701991a412ec37a7b31530def]
    Former-commit-id: 504e6b419a48264a73e614dd94162f41d5cd858f

---
+++ Domain_Objects/src/main/java/potatoes/project/domain_objects/Filmography.java
@@ -0,0 +1,37 @@
+package potatoes.project.domain_objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Filmography {
+
+	@OneToOne
+	Media media;
+	@OneToOne
+	Celebrity celebrity;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+	public Filmography() {}
+	
+	public Filmography(Media media, Celebrity celebrity) {
+		this.media=media;
+		this.celebrity=celebrity;
+	}
+	public Media getMedia() {
+		return media;
+	}
+
+	public Celebrity getCelebrity() {
+		return celebrity;
+	}
+	
+	
+}
