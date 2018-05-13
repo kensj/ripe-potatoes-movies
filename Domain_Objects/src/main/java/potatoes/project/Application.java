7f50bdfa47bb27be1d5831cbc23378e31d7a8999
kensj <kenji.japra@gmail.com>
2018-05-13 19:46:52 -0400

    Merge
    Former-commit-id: e46f6e2367d46d7b76623bd3f1be424cf8d24525 [formerly 8d6f7c2a35e70d066959e564c2081c592d125564]
    Former-commit-id: 9c3c1562d7ba5e91f6511431cc2671a51c4db3f1

--- Domain_Objects/src/main/java/potatoes/project/Application.java
+++ Domain_Objects/src/main/java/potatoes/project/Application.java
@@ -16,7 +16,7 @@ import potatoes.project.storage.StorageService;
 public class Application {
 	
 	public static void main(String[] args) { SpringApplication.run(Application.class, args); }
-	
+
 	@Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> { storageService.init(); };
