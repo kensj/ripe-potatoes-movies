31af44e2dc5b058d00acca451fa15962eb4494cd
kensj <kenji.japra@gmail.com>
2018-05-14 02:49:29 -0400

    Change email and change password working
    Former-commit-id: 6abb79b744935cf3e820c3338a399684763ad901 [formerly 47a786a473d60c9abcadd9c8101a07073611caae]
    Former-commit-id: d2f68709f4a200d1a289eb8324ad0e1778dc3876

--- Domain_Objects/src/main/resources/static/js/profile.js
+++ Domain_Objects/src/main/resources/static/js/profile.js
@@ -30,6 +30,10 @@ function openM() {
 	$("#registerFrame").hide();
 	$("#loginFrame").hide();
 	$("#manageAccount").show();
+
+	$(".emailChangedText").hide();
+	$(".passwordChangedText").hide();
+	
 	document.getElementById('id01').style.display='block';
 }
 $(".deleteAccount").click(function() {
@@ -280,4 +284,80 @@ function deleteIcon() {
             console.log("An error occurred whilst trying to contact the server: " + jQXHR.status + " " + textStatus + " " + errorThrown);
         }
     });
+}
+
+function manageAccount() {
+	
+	var token = $("meta[name='_csrf']").attr("content");
+	var header = $("meta[name='_csrf_header']").attr("content");
+	
+	var email = $('#manageEmail').val().trim();
+	var cpw = $('#manageCPW').val().trim();
+	var pw1 = $('#managePW1').val().trim();
+	var pw2 = $('#managePW2').val().trim();
+	
+	if(!!email){
+		$.ajax({
+	    	headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			type: 'POST',
+	        url: '/update-email?password=' + cpw + '&newEmail=' + email,
+	        cache: false,
+	        beforeSend: function(xhr) {
+				xhr.setRequestHeader(header, token);
+			},
+	        success: function (response, succ, value) {	        	
+	        	if(response.success == "true") {
+		        	$("#manageAccount").hide();
+		        	document.getElementById('id01').style.display='none';
+		        	$(".emailChangedText").show();
+		        	$("#manageEmail").attr("placeholder",email);
+	        	}
+	        	else {
+	        		alert("Error changing email")
+	        	}
+	        },
+	        error: function (jQXHR, textStatus, errorThrown) {
+	            alert("Unknown error")
+	        }
+	    });
+	}
+	
+	if(!!pw1 && pw1==pw2){
+		$.ajax({
+	    	headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			type: 'POST',
+	        url: '/update-password?oldPass=' + cpw + '&newPass=' + pw1,
+	        cache: false,
+	        beforeSend: function(xhr) {
+				xhr.setRequestHeader(header, token);
+			},
+	        success: function (response, succ, value) {
+	        	if(response.success == "true") {
+		        	$("#manageAccount").hide();
+		        	document.getElementById('id01').style.display='none';
+		        	$(".passwordChangedText").show();
+	        	}
+	        	else {
+	        		alert("Error changing password")
+	        	}
+	        },
+	        error: function (jQXHR, textStatus, errorThrown) {
+	            alert("Unknown error")
+	        }
+	    });
+	}
+	else if(pw1!=pw2) {
+		alert("Passwords don't match")
+	}
+	
+	$('#manageEmail').val("");
+	$('#manageCPW').val("");
+	$('#managePW1').val("");
+	$('#managePW2').val("");
 }
